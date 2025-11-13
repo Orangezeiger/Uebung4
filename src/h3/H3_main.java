1fb6039713f310f7ac546997b681ac57001ac747
Max <maxsiepsiak238@gmail.com>
2025-11-13 14:42:29 +0100

    H3 1.1

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,9 +2,13 @@ package h3;
 
 public class H3_main {
     public static void main(String[] args) {
-        float celsiusFloat = 22.0F, fahrenheitFloat = 68.3F;
-        double fahrenheitDouble = 22.0, celsiusDouble = 55.3;
+        float celsiusFloat = 14.2F, fahrenheitFloat = 68.3F;
+        double fahrenheitDouble = 22.0, celsiusDouble = 14.2;
 
+        fahrenheitFloat = celsiusFloat * (9.0F / 5.0F) + 32;
+        fahrenheitDouble = celsiusDouble * (9.0 / 5.0) + 32;
 
+        System.out.println("Fahrenheit (Float): " + fahrenheitFloat);
+        System.out.println("Fahrenheit (Double): " + fahrenheitDouble);
     }
 }
