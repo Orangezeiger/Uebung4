3bc2a517b4b4d22f38eeb100dfb16e443c329bca
Max <maxsiepsiak238@gmail.com>
2025-11-13 14:24:54 +0100

    Init h3

---
+++ src/h3/H3_main.java
@@ -0,0 +1,10 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        float celsiusFloat = 22.0F, fahrenheitFloat = 68.3F;
+        double fahrenheitDouble = 22.0, celsiusDouble = 55.3;
+
+
+    }
+}
